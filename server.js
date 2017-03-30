53dce05032af5b683cf92bc6a21fdf4e8132b871
Kenneth Rasmussen <kenneth.rasmussen@capgeminisogeti.dk>
2017-03-30 20:42:45 +0200

    Mock reply

--- server.js
+++ server.js
@@ -11,8 +11,34 @@ app.use(bodyParser.urlencoded());
 
 app.post("/food", function(request, response){
     console.log(request.body);
-    response.end("test");
+
+    var now = Date.now();
+
+    var split = request.body.text.split("@");
+    var orderFrom = split[0];
+    var orderAtString = split[1];
+    var orderAtArray = orderAtString.split(":");
+    var orderAt = Date.now().setHours(orderAtArray[0], orderAtArray[1]);
+
+    response.setHeader("content-type", "application/json");
+    response.end(`{
+        "response_type": "in_channel",
+        "text": "` + request.body.user_name + ` orders from ` + orderFrom + ` at ` + orderAtString + ` (In ` + timeDifferenceString(now, orderAt) + `)",
+    }`);
 });
+
+function timeDifferenceString(date1, date2){
+    var timeDiff = Math.abs(date1.getTime() - date2.getTime());
+    var hours = Math.ceil(timeDiff / (1000 * 3600));
+    var minutes = Math.ceil((timeDiff % (1000 * 3600)) / (1000 * 60));
+
+    var s = "";
+    if(hours > 0){
+        s += hours + " timer"
+    }
+    s += " og " + minutes + " minutter";
+    return s;
+}
 
 app.listen(PORT, function(){
 	console.log("Listening on: " + PORT);
